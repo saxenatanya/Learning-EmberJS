4588e038e582f2b795f37d91c598d95e8197d81a
Tanya Saxena <82470912+saxenatanya@users.noreply.github.com>
2023-07-27 07:21:55 +0000

    product details added

--- app/routes/cart.js
+++ app/routes/cart.js
@@ -1,3 +1,15 @@
 import Route from '@ember/routing/route';
 
-export default class CartRoute extends Route {}
+export default class CartRoute extends Route {
+  model() {
+    const items = [{ price: 10 }, { price: 15 }];
+    return items;
+  }
+
+  setupController(controller, model) {
+    super.setupController(controller, model);
+    const subTotal = model.reduce((acc, item) => {
+      return acc + item.price;
+    }, 0);
+  }
+}
